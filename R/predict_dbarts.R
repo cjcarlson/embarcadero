7d1338ce050bae2c7e7559a1dee7509bc8899517
CJ Carlson <cjcarlson@berkeley.edu>
2019-06-25 06:13:54 -0400

    posterior draws

--- R/predict_dbarts.R
+++ R/predict_dbarts.R
@@ -10,22 +10,49 @@
 #' @export
 #'
 
-predict.dbart.raster <- function(model, inputstack, cores=3) {
+predict.dbart.raster <- function(model, inputstack, ci=FALSE) {
 
   input.matrix <- as.matrix(getValues(inputstack))
   output = predict(model,
                    input.matrix)
   output = pnorm(output)
-  results <- inputstack
   output.m <- t(matrix(colMeans(output),
-                       nrow = ncol(results),
-                       ncol = nrow(results)))
+                       nrow = ncol(inputstack),
+                       ncol = nrow(inputstack)))
   output.r <- raster(output.m,
                      xmn=xmin(inputstack[[1]]), xmx=xmax(inputstack[[1]]),
                      ymn=ymin(inputstack[[1]]), ymx=ymax(inputstack[[1]]),
                      crs=inputstack[[1]]@crs) + inputstack[[1]]*0
 
-  plot(output.r)
-  return(output.r)
+  if(ci==TRUE) {
+
+    output.m.U <- t(matrix(apply(output, 2, function(x) quantile(x, probs=0.95)),
+                         nrow = ncol(inputstack),
+                         ncol = nrow(inputstack)))
+    output.r.U <- raster(output.m.U,
+                       xmn=xmin(inputstack[[1]]), xmx=xmax(inputstack[[1]]),
+                       ymn=ymin(inputstack[[1]]), ymx=ymax(inputstack[[1]]),
+                       crs=inputstack[[1]]@crs) + inputstack[[1]]*0
+
+
+    output.m.L <- t(matrix(apply(output, 2, function(x) quantile(x, probs=0.05)),
+                           nrow = ncol(inputstack),
+                           ncol = nrow(inputstack)))
+    output.r.L <- raster(output.m.L,
+                         xmn=xmin(inputstack[[1]]), xmx=xmax(inputstack[[1]]),
+                         ymn=ymin(inputstack[[1]]), ymx=ymax(inputstack[[1]]),
+                         crs=inputstack[[1]]@crs) + inputstack[[1]]*0
+
+
+    par(mfrow=c(2,2))
+    plot(output.r,main="Mean")
+    plot(output.r.U-output.r.L,main="95% CI range")
+    plot(output.r.L,main='Lower 95% CI')
+    plot(output.r.U,main='Upper 95% CI')
+
+    return(list(mean=output.r, upper.ci=output.r.U, lower.ci=output.r.L))
+
+  } else { plot(output.r); return(output.r) }
+
 
 }
