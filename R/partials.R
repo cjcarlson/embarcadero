6fc6f9a73677e3e2d144118fdad11da245da0902
CJ Carlson <cjcarlson@berkeley.edu>
2019-07-18 16:28:23 -0400

    partials with nice posterior visualization!!
    kool aid man voice oh yes

--- R/partials.R
+++ R/partials.R
@@ -5,7 +5,7 @@
 #' I don't really like the partial visualizations in dbarts so I made some of my own, with a little more customization options.
 #'
 #' @param model A dbarts model object
-#' @param xind A list of the variables for which you want to run the partials. Defaults to doing all of them.
+#' @param x.vars A list of the variables for which you want to run the partials. Defaults to doing all of them.
 #' @param equal Spacing x levels equally instead of using quantiles
 #' @param smooth A multiplier for how much smoother you want the sampling of the levels to be.
 #' @param posterior The way the posterior is visualized: defaults to nothing, but can do "CI" for the credible interval or "trace" for traceplots
@@ -13,55 +13,99 @@
 #'
 #' @return Returns a nice plot
 #'
-#' @example
-#'
+#' @examples
 #' f <- function(x) { return(0.5 * x[,1] + 2 * x[,2] * x[,3]) }
-#'  sigma <- 0.2
-#' n     <- 100
-#' set.seed(27)
+#' sigma <- 0.2
+#' n <- 100
 #' x <- matrix(2 * runif(n * 3) -1, ncol = 3)
 #' colnames(x) <- c('rob', 'hugh', 'ed')
 #' Ey <- f(x)
 #' y  <- rnorm(n, Ey, sigma)
 #' df <- data.frame(y, x)
 #' set.seed(99)
+#' 
 #' bartFit <- bart(y ~ rob + hugh + ed, df,
 #'                keepevery = 10, ntree = 100, keeptrees = TRUE)
 #'
-#'
+#' partial(bartFit, x.vars=c('hugh'), equal=TRUE, smooth=10, trace=TRUE, ci=TRUE)
+#' partial(bartFit, x.vars=c('rob'), equal=TRUE, smooth=10, trace=FALSE, ci=TRUE)
+#' partial(bartFit, x.vars=c('ed'), equal=TRUE, smooth=10, trace=TRUE, ci=FALSE)
 #'
 #' @export
 #'
 #'
 
 partial <- function(model, x.vars=NULL, equal=FALSE, smooth=1,
-                    posterior=NULL) {
+                    posterior=NULL, ci=TRUE, trace=TRUE) {
   
 # This is for something else ultimately: attr(bartFit$fit$data@x, "term.labels")
 # This is where equal happens
   
 if (is.null(x.vars)) { raw <- bartFit$fit$data@x} else ( raw <- bartFit$fit$data@x[,x.vars])
-
-
-if(equal==TRUE) {
-    minmax <- data.frame(mins = apply(raw, 2, min),
+  
+  if(equal==TRUE) {
+    if(!is.null(x.vars) && length(x.vars)==1) {
+          minmax <- data.frame(mins = min(raw),
+                               maxs = max(raw)) } else {
+          minmax <- data.frame(mins = apply(raw, 2, min),
                          maxs = apply(raw, 2, max))
+    }
     lev <- lapply(c(1:nrow(minmax)), function(i) {seq(minmax$mins[i], minmax$maxs[i], (minmax$maxs[i]-minmax$mins[i])/(10*smooth))})
-    pd <- pdbart(model, xind = x.vars, levs = lev, pl=TRUE)
-} else {
-  levq = c(0.05, seq(0.1, 0.9, 0.1/smooth), 0.95)
-  pd <- pdbart(model, xind = x.vars, levquants = levq, pl=TRUE)
+    pd <- pdbart(model, xind = x.vars, levs = lev, pl=FALSE)
+  } else {
+    levq = c(0.05, seq(0.1, 0.9, 0.1/smooth), 0.95)
+    pd <- pdbart(model, xind = x.vars, levquants = levq, pl=FALSE)
+  }
+ 
+  
+# This is the wrapper itself
+  
+for (i in 1:length(pd$fd)) {
+  
+  q50 <- apply(pd$fd[[i]],2,median)
+  if(transform==TRUE) {q50 <- pnorm(q50)}
+  
+  df <- data.frame(x=pd$levs[[i]],med=q50)
+
+  if(ci==TRUE) {
+    q05 <- apply(pd$fd[[i]],2,quantile,probs=0.05)
+    if(transform==TRUE) {q05 <- pnorm(q05)}
+    q95 <- apply(pd$fd[[i]],2,quantile,probs=0.95)
+    if(transform==TRUE) {q95 <- pnorm(q95)}
+    df$q05 <- q05
+    df$q95 <- q95
+  }
+  
+  if(trace==TRUE) {
+    f <- data.frame(t(pd$fd[[i]]))
+    df <- cbind(df, f)
+  }
+      
+  g <- ggplot(df,aes(x=x, y=med)) + 
+    labs(title=pd$xlbs[[i]], y='Response',x='') + theme_light(base_size = 20) + 
+    theme(plot.title = element_text(hjust = 0.5),
+          axis.title.y = element_text(vjust=1.7))
+  
+  if(ci==TRUE) {alpha2 <- 0.05; k <- 4} else {alpha2 <- 0.1; k <- 2}
+  if(trace==TRUE) {
+    if(transform==TRUE) {
+      for(j in 1:nrow(pd$fd[[i]])) {
+        g <- g + geom_line(aes_string(y=pnorm(df[,j+k])), alpha=alpha2)
+      }
+    } else {
+      for(j in 1:nrow(pd$fd[[i]])) {
+        g <- g + geom_line(aes_string(y=df[,j+k]), alpha=alpha2)
+      }
+    }
+  }
+  
+  if(ci==TRUE) {
+    g <- g + geom_ribbon(aes(ymin=q05, ymax=q95), fill='deepskyblue1', alpha=0.3)
+  }
+  
+  g <- g + geom_line(size=1.25)
+  
+  print(g)
+
 }
-  
-# This is the wrapper itself
-  
-#for (i )
-# as.name
-#}
-
-# This is the wrapper itself
-pd <- pdbart(model, xind = x.vars, levs = lev, pl=TRUE)
-#for (i )
-# as.name
-
 }
