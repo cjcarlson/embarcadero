433376ac61701fe4d1781c5bd5179b1bcf14a902
CJ Carlson <cjcarlson@berkeley.edu>
2019-07-18 14:36:36 -0400

    this is the worst day of my life
    STOP DELETING MY CODE

--- R/partials.R
+++ R/partials.R
@@ -3,3 +3,26 @@
 # add levels to it option
 # add an option for CI this time
 # xind using as.name
+
+partial <- function(model, x.vars=NULL, equal=FALSE, smooth=1,
+                    posterior=NULL) {
+  
+# This is for something else ultimately: attr(bartFit$fit$data@x, "term.labels")
+  
+# This is where equal happens
+if (is.null(x.vars)) { raw <- bartFit$fit$data@x} else ( raw <- bartFit$fit$data@x[,x.vars])
+
+if(equal==TRUE) {
+    minmax <- data.frame(mins = apply(raw, 2, min),
+    maxs = apply(raw, 2, max))
+    lev <- lapply(c(1:nrow(minmax)), function(i) {seq(minmax$mins[i], minmax$maxs[i], (minmax$maxs[i]-minmax$mins[i])/(10*smooth))})
+} else {
+    lev <- NULL
+}
+  
+# This is the wrapper itself
+pd <- pdbart(model, xind = x.vars, levs = lev, pl=TRUE)
+#for (i )
+# as.name
+
+}
