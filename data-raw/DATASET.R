6014e9b7b3289b6fdca0134cef6ac8de4b4d625d
CJ Carlson <cjcarlson@berkeley.edu>
2019-09-12 15:44:27 -0400

    SKRRRRRRRRRRRRRRRRRRRRRT
    hadley whickam is the guy who invented going "skrrrrrrt"

---
+++ data-raw/DATASET.R
@@ -0,0 +1,8 @@
+## code to prepare `DATASET` dataset goes here
+
+covsraw <- raster::stack(list.files(pattern='.asc',
+                         full.names = TRUE))
+covsraw <- brick(covsraw)
+covsraw <- stack(covsraw)
+
+usethis::use_data("covsraw")
