b93859b44d9136acb50a01a3d874187d7ce8f86a
CJ Carlson <cjcarlson@berkeley.edu>
2019-12-09 10:22:41 -0500

    chunky is bigger. he's powerful now
    predict2 fixes (1) the need to be able to divide up datasets and iteratively predict for memory management [this still needs a progress bar of some kind] (2) the need to be able to specify quantiles [multiple new libraries used here - please add them to the dependencies] and (3) the need to exclude NAs in the prediction step for both memory management and the NAs-become-zeros issue internal to dbarts.

--- R/chunky.R
+++ R/chunky.R
@@ -1,8 +1,72 @@
 
-chunky <- function(mod,
-                   data) {
-  
-  input.str <-  str(split(data, (as.numeric(rownames(data))-1) %/% 200))
+library(data.table)
+library(matrixStats)
 
+model <- sdm
+inputstack <- bigstack
+splitby=5
+quantiles=c(0.05,0.95)
+inputstack[1] <- NA
+
+dfextract <- function(df, quant) {
+  if(length(quant)==0) {return(colMeans(df))} else 
+  return(cbind(data.frame(colMeans(df)),
+               colQuantiles(df, probs=quant)))
+}
+
+############################
+
+predict2 <- function(model, 
+                     inputstack,
+                     splitby=1,
+                     quantiles=c()) {
   
+  xnames <- attr(model$fit$data@x, "term.labels")
+  if(all(xnames %in% names(inputstack))) {
+    inputstack <- inputstack[[xnames]]
+  } else {
+    stop("Variable names of RasterStack don't match the requested names")
+  }
+  input.matrix <- as.matrix(getValues(inputstack))
+  
+  blankout <- data.frame(matrix(ncol=(1+length(quantiles)), nrow=ncell(inputstack[[1]])))
+  whichvals <- which(complete.cases(input.matrix))
+  input.matrix <- input.matrix[complete.cases(input.matrix),]
+  
+  if(splitby==1) {
+    pred <- predict(model, input.matrix)
+    pred.summary <- dfextract(pred, quant=quantiles)
+  } else {
+    split <- floor(nrow(input.matrix)/splitby)
+    input.df <- data.frame(input.matrix)
+    input.str <- split(input.df, (as.numeric(1:nrow(input.df))-1) %/% split)
+    for(i in 1:length(input.str)){
+        pred <- predict(model, input.str[[i]])
+        pred.summary <- dfextract(pred, quant=quantiles)
+        input.str[[i]] <- pred.summary
+    }
+    if(length(quantiles)==0) {
+      pred.summary <- data.frame(means=unlist(input.str)) } else {
+        pred.summary <- rbindlist(input.str)
+      }
+  }
+  
+  pred.summary <- as.matrix(pred.summary)
+  output = pnorm(pred.summary)
+  
+  blankout[whichvals,] <- output
+  output <- blankout
+  
+  outlist <- lapply(1:ncol(output), function(x) {
+      output.m <- t(matrix(output[,x],
+                       nrow = ncol(inputstack),
+                       ncol = nrow(inputstack)))
+      return(raster(output.m,
+                     xmn=xmin(inputstack[[1]]), xmx=xmax(inputstack[[1]]),
+                     ymn=ymin(inputstack[[1]]), ymx=ymax(inputstack[[1]]),
+                     crs=inputstack[[1]]@crs))
+  })
+  
+  outlist <- stack(outlist)
+  return(outlist)
 }
