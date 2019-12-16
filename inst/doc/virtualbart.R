f2a1a85556138881e7b2a710df7db34dacfc5bc6
CJ Carlson <cjcarlson@berkeley.edu>
2019-12-16 11:00:29 -0500

    Skeleton of reassembled vignette

---
+++ inst/doc/virtualbart.R
@@ -0,0 +1,12 @@
+## ---- include = FALSE----------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+suppressMessages(library(tidyverse))
+
+## ----setup, echo=FALSE---------------------------------------------------
+library(embarcadero)
+library(velox)
+set.seed(12345)
+
