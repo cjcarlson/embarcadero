98e3389201428c1c11b5101cb19f9a0e6551ed1b
CJ Carlson <cjcarlson@berkeley.edu>
2019-12-25 21:48:38 -0500

    chunk names

--- inst/doc/virtualbart.R
+++ inst/doc/virtualbart.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE----------------------------------------------------
+## ----knitr, include = FALSE----------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -15,7 +15,7 @@ library(NLMR, quietly = T)
 library(virtualspecies, quietly = T)
 set.seed(42)
 
-## ------------------------------------------------------------------------
+## ----neutral-landscape---------------------------------------------------
 onelandscape <- function(x) {NLMR::nlm_gaussianfield(nrow = 150,
                                                      ncol = 150,
                                                      rescale = FALSE)}
@@ -26,7 +26,7 @@ names(climate) <- xnames
 plot(climate[[1]],main='An imaginary variable', 
      box=F, axes=F)
 
-## ------------------------------------------------------------------------
+## ----random-species------------------------------------------------------
 
 # Generate the species' climatic niche
 
@@ -56,7 +56,7 @@ occ.df <- cbind(sp.points$sample.points,
 occ.df <- occ.df[,-c(1:3)]
 
 
-## ------------------------------------------------------------------------
+## ----basic-bart----------------------------------------------------------
 # Check out the data structure
 head(occ.df)
 
@@ -78,11 +78,11 @@ plot(random.sp$pa.raster, main='True distribution',
 plot(map, main='Predicted probability', 
      box=F, axes=F)
 
-## ------------------------------------------------------------------------
+## ----basic-bart-summary--------------------------------------------------
 # How good is it?
 summary(sdm) 
 
-## ------------------------------------------------------------------------
+## ----basic-plots---------------------------------------------------------
 # How's it look?
 par(mfrow=c(1,2))
 plot(random.sp$pa.raster, main='True distribution', 
@@ -90,7 +90,7 @@ plot(random.sp$pa.raster, main='True distribution',
 plot(map>0.42, main='Prediced distribution', 
      box=F, axes=F)
 
-## ------------------------------------------------------------------------
+## ----uncertainty---------------------------------------------------------
 
 # Predict the species distribution!
 map <- predict(sdm, climate, quantiles=c(0.025, 0.975), quiet=TRUE)
@@ -107,10 +107,10 @@ plot(map[[3]], 'Upper 95% CI bound',
 plot(map[[3]]-map[[2]], 'Credible interval width', 
      box=F, axes=F)
 
-## ------------------------------------------------------------------------
+## ----plot-mcmc-----------------------------------------------------------
 plot.mcmc(sdm, climate, iter=5, quiet = TRUE)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ----plot-mcmc-fewer-trees, eval=FALSE-----------------------------------
 #  # Retrain the model
 #  sdm.tiny <- bart(y.train=occ.df[,'Observed'],
 #              x.train=occ.df[,xnames],
@@ -119,17 +119,17 @@ plot.mcmc(sdm, climate, iter=5, quiet = TRUE)
 #              nskip=0) # No burnin
 #  plot.mcmc(sdm.tiny, climate, iter=100)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ----animate, eval=FALSE-------------------------------------------------
 #  library(animation)
 #  saveGIF(plot.mcmc(sdm, climate, iter=50), movie.name = "Timelapse.gif", interval = 0.15,
 #    ani.width = 800, ani.height = 400)
 
-## ------------------------------------------------------------------------
+## ----varimp-diag---------------------------------------------------------
 # A variable importance diagnostic. What's behaving well?
 # This takes a while to run normally! Drop the iter if you want a plot faster with more variance.
 varimp.diag(occ.df[,xnames], occ.df[,'Observed'], iter=50, quiet=TRUE)
 
-## ------------------------------------------------------------------------
+## ----stepwise-bart-------------------------------------------------------
 # Stepwise variable set reduction
 step.model <- variable.step(x.data=occ.df[,xnames], 
                             y.data=occ.df[,'Observed'],
@@ -150,30 +150,29 @@ plot(random.sp$pa.raster, main='True distribution',
 plot(map, main='Predicted probability', 
      box=F, axes=F)
 
-## ------------------------------------------------------------------------
+## ----stepwise-bart-varimp------------------------------------------------
 varimp(sdm, plot=TRUE)
 
-## ------------------------------------------------------------------------
+## ----virtual-species-gaussian--------------------------------------------
 par(mfrow=c(1,1))
 plotResponse(random.sp)
 
-## ------------------------------------------------------------------------
+## ----basic-partial-------------------------------------------------------
 p <- partial(sdm, x.vars=c('x4'),
         trace=FALSE,
         ci=FALSE)
 
-## ------------------------------------------------------------------------
+## ----basic-partial-trace-------------------------------------------------
 p <- partial(sdm, x.vars=c('x4'),
         trace=TRUE,
         ci=FALSE)
 
-## ------------------------------------------------------------------------
+## ----basic-partial-ci----------------------------------------------------
 p <- partial(sdm, x.vars=c('x4'),
         trace=FALSE,
         ci=TRUE)
-p
 
-## ------------------------------------------------------------------------
+## ----basic-partial-ggplot------------------------------------------------
 p <- partial(sdm, x.vars=c('x4'),
         trace=FALSE,
         ci=TRUE,
@@ -182,7 +181,7 @@ p <- partial(sdm, x.vars=c('x4'),
 
 p[[1]] + ggtitle('Partial plot') + labs(x='x4')
 
-## ------------------------------------------------------------------------
+## ----basic-partial-panels------------------------------------------------
 p <- partial(sdm,
         trace=FALSE,
         ci=TRUE,
@@ -191,7 +190,7 @@ p <- partial(sdm,
         panels=TRUE)
 p
 
-## ------------------------------------------------------------------------
+## ----gbm-partial---------------------------------------------------------
 gbm1 <- gbm.step(data=occ.df, gbm.x = 2:5, gbm.y = 1, 
                  family = "bernoulli",
                  tree.complexity = 5, 
@@ -202,7 +201,7 @@ gbm.plot(gbm1, variable.no=4, rug=TRUE,
          #main="BRT partial",
          plot.layout=c(1,1))
 
-## ------------------------------------------------------------------------
+## ----spartial------------------------------------------------------------
 spartial(sdm, climate, x.vars='x4',
          equal=TRUE, smooth=5)
 
