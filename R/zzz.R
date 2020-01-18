82688985c38409dae9abf4c388591d02bf294c4b
CJ Carlson <cjcarlson@berkeley.edu>
2020-01-18 17:44:40 -0500

    version 2

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,7 @@
   setOldClass("bart")
   setMethod("predict", "bart", predict2.bart)
   setMethod("summary", "bart", summary.bart)
+  setMethod("predict", "rbart", predict2.bart)
+  setMethod("summary", "rbart", summary.bart)
   invisible()
 }
